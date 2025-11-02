e749460bca31a0d10f477b04301b3e8bb741a3d3
unknown <suraz.kuru@gmail.com>
2025-11-02 18:45:58 +0545

    db connection

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config()
+import mongoose from "mongoose";
+import {DB_NAME} from "./src/constants.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./src/db/DB.js";
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello World from Express.js');
-})
+connectDB();
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         console.log("Connected to the database successfully");
+//         app.on("error", (error) => {
+//             console.error("Error occurred in the server", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("Error connecting to the database", error);
+//     }
+// })()
